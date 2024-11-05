ea5abc38da84ee64e80a15640aaf73e49d28f3a7
mohamed hassan <hh71728@gmail.com>
2024-11-05 13:35:01 +0300

    0.02- added websocket

--- nery/yi.go
+++ nery/yi.go
@@ -1,15 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// read message from client
+		_, message, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		// show message
+		log.Printf("Received message: %s", message)
+
+		//send message to client
+		err = conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/websocket", websocketHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
